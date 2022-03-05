b2f912774b3411e397ef32a76cea43df1fb32d5f
ljunggren <mljunggren@gmail.com>
2022-03-05 13:58:40 -0500

    Added baseline

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Set the 'production' environment configuration object
+module.exports = {
+  DEFAULT_PROTOCOL:"http://",
+  SERVER_IP:"192.168.1.11",
+  PORT:80
+};
