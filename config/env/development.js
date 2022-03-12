8797053827025f2f5b53e88ac468cdb4efcb3c05
ljunggren <mljunggren@gmail.com>
2022-03-12 17:16:46 -0500

    Added localhost

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@
 // Set the 'production' environment configuration object
 module.exports = {
   DEFAULT_PROTOCOL:"http://",
-  SERVER_IP:"192.168.1.11",
+  SERVER_IP:"localhost",
   PORT:80,
   HTTPS_PORT:443
 };
